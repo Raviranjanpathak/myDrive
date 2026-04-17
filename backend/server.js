204cf95eb9d47c8bb4f917a58fa41c944ee46a2a
Raviranjan Pathak <raviranjanpathak750@gmail.com>
2026-04-17 18:06:24 +0530

    Added root route

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,9 @@ app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
 //  Routes
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/folders", require("./routes/folderRoutes"));
 app.use("/api/files", require("./routes/fileRoutes"));
